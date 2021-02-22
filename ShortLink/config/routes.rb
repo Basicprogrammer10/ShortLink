17fa9fffd89b524bf7324c249b17ad729f26947c
Connor Slade <turtleslade@gmail.com>
2021-02-21 23:27:32 -0500

    things

--- ShortLink/config/routes.rb
+++ ShortLink/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'pages#create'
   get 'pages/create'
 
-  #get 'pages/response'
+  get '/create' => 'pages#create'
   get '/response' => 'pages#response'
+  get '/r/:code' => 'pages#r'
 end

e5d4d5703738d32d6f2ef0a7fd2dc4e1c3318f8d
Connor Slade <turtleslade@gmail.com>
2021-02-21 22:25:42 -0500

    : /

--- ShortLink/config/application.rb
+++ ShortLink/config/application.rb
@@ -1,6 +1,7 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
+require 'sqlite3'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,5 +19,16 @@ module ShortLink
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.after_initialize do
+      $db = if File.exist?('test.db')
+              SQLite3::Database.open('test.db')
+            else
+              puts 'Creating database...'
+              SQLite3::Database.new('test.db')
+            end
+
+      $db.execute('CREATE TABLE IF NOT EXISTS data(code TEXT, url TEXT)')
+      $db.results_as_hash = true
+    end
   end
 end
